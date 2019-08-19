a70b869ff5f5f848abf4b41b2a8207f519856571
x363090973 <363090973@qq.com>
2019-08-19 17:15:59 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+src="../assets/logo.png"
+         alt="Vue logo">
+    <HelloWorld :msg="opti" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
+  name: 'Home',
   components: {
-    HelloWorld,
+    HelloWorld
   },
-};
+  data: () => {
+    return {
+      opti: 'ssss'
+    }
+  }
+}
 </script>
